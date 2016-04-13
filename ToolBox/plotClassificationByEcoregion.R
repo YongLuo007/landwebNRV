05b33270a65758d0904fc7612a9ab86a7770c5c5
Yong Luo <yluo1@lakeheadu.ca>
2016-04-13 00:57:42 -0700

    starting a new function

---
+++ ToolBox/plotClassificationByEcoregion.R
@@ -0,0 +1,11 @@
+# this function is to classify ground plots
+# by ecoregion in the study area
+
+plotClassificationByEcoregion <- function(plotLocation, studyAreaEcoregion){
+  
+  
+  
+  
+  
+  
+}
