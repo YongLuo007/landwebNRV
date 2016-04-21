8d2f009cfb868073ec9d680a8a2595060e1816ab
Yong Luo <yluo1@lakeheadu.ca>
2016-04-21 00:15:38 -0700

    purifying BC PSP data

--- landwebNRV/R/dataPurification_BCMature.R
+++ landwebNRV/R/dataPurification_BCMature.R
@@ -21,7 +21,7 @@
 #' @include 
 #' @export
 #' @docType methods
-#' @rdname dataPurification_BCMature
+#' @rdname dataPurification_BC
 #'
 #' @author Yong Luo
 #'
@@ -29,17 +29,35 @@
 #' \dontrun{
 #' 
 #' }
-setGeneric("dataPurification_BCMature", function(treeDataRaw, headDataRaw) {
-  standardGeneric("dataPurification_BCMature")
+setGeneric("dataPurification_BC", function(treeDataRaw, headDataRaw) {
+  standardGeneric("dataPurification_BC")
 })
 #' @export
-#' @rdname dataPurification_BCMature
+#' @rdname dataPurification_BC
 setMethod(
-  "dataPurification_BCMature",
+  "dataPurification_BC",
   signature = c(treeDataRaw = "data.table", 
                 headDataRaw = "data.table"),
   definition = function(treeDataRaw, headDataRaw) {
+    # purifying headData
+    # like AB data, the BC PSP have two level heirachical design (SAMP_ID and plot)
+    # get the plots with locations
+    headDataRaw[,':='(utmtimes = length(unique(utm_zone)),
+                      eastingtimes = length(unique(utm_easting)),
+                      northingtimes = length(unique(utm_northing)),
+                      SAtimes = length(unique(tot_stand_age)),
+                      plotsizetimes = length(unique(area_pm))),
+                by = SAMP_ID]
+    # unique(headDataRaw$utmtimes)
+    # unique(headDataRaw$eastingtimes)
+    # unique(headDataRaw$northingtimes)
+    # unique(headDataRaw$SAtimes)
+    # unique(headDataRaw$plotsizetimes)
+    unique(headDataRaw[SAtimes == 2,]$SAMP_ID)[1]
+    headDataRaw[SAMP_ID == "3251_0012_VRI", ]
     
+    headData <- headDataRaw[!is.na(utm_zone) & !is.na(utm_easting) & !is.na(utm_northing),]
+    # get the plots with SA
     
     
     
