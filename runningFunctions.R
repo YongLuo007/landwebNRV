7fdfb9f32fd643038057d803da9b78784eedb60c
Yong Luo <yluo1@lakeheadu.ca>
2016-04-25 18:03:57 -0700

    running fuction

--- runningFunctions.R
+++ runningFunctions.R
@@ -71,19 +71,28 @@ headdataraw <- read.csv("C:/Users/yonluo/Documents/LandWeb/Data/AB/plotLocation.
                         header = TRUE,
                         stringsAsFactor = FALSE) %>%
   data.table
-
 source('~/GitHub/landwebNRV/landwebNRV/R/dataPurification_ABMature.R')
 ls()
-output <- dataPurification_ABMature(treeDataRaw = treedataraw, headDataRaw = headdataraw)
+output <- dataPurification_ABMature(treeDataRaw = treedataraw, plotHeaderDataRaw = headdataraw)
 
 
 
 rm(list=ls())
-source('~/GitHub/landwebNRV/landwebNRV/R/dataPurification_BCMature.R')
+source('~/GitHub/landwebNRV/landwebNRV/R/dataPurification_BCPSP.R')
 setwd("C:/Users/yonluo/Documents/LandWeb/Data/BC")
 load("BC_PSP.RData")
+dd <- dataPurification_BCPSP(treeDataRaw = treedata, plotHeaderDataRaw = plotheader)
+treeData <- dd$treeData
 
-dd <- dataPurification_BC(treeDataRaw = treedata, headDataRaw = plotheader)
+source('~/GitHub/landwebNRV/landwebNRV/R/standardizeSpeciesName.R')
+
+treeData1 <- standardizeSpeciesName(speciesTable = treeData, forestInventorySource = "BCPSP")
+
+source('~/GitHub/landwebNRV/landwebNRV/R/biomassCalculation.R')
+treeData1[, Biomass:=biomassCalculation(species = newSpeciesName, DBH = DBH)$biomass]
+
+Biomass <- biomassCalculation(species = treeData1$newSpeciesName, DBH = treeData1$DBH)
+
 
 rm(list=ls())
 setwd("~/LandWeb/Data/SK")
@@ -94,21 +103,21 @@ plotHeadRaw <- plotheader3
 measureHeadRaw <- plotheader2
 treeDataRaw <- treedata
 
-dd <- dataPurification_SKPSP(SADataRaw = SADataRaw, plotHeadRaw = plotHeadRaw,
-                             measureHeadRaw = measureHeadRaw, treeDataRaw= treeDataRaw)
+dd <- dataPurification_SKPSP(SADataRaw = SADataRaw, plotHeaderRaw = plotHeadRaw,
+                             measureHeaderRaw = measureHeadRaw, treeDataRaw= treeDataRaw)
 
 
 rm(list=ls())
 setwd("~/LandWeb/Data/SK")
 load("SKTSP_PAPP.RData")
-source('~/GitHub/landwebNRV/landwebNRV/R/dataPurification_SKTSP.R')
+source('~/GitHub/landwebNRV/landwebNRV/R/dataPurification_SKTSP_PPPA.R')
 
-PPoutput <- dataPurification_SKTSP(sampleTreeRaw = PPsampletree,
-                             plotHeadRaw = PPplotheader,
+PPoutput <- dataPurification_SKTSP_PPPA(sampleTreeRaw = PPsampletree,
+                             plotHeaderRaw = PPplotheader,
                              treeDataRaw = PPtreedata)
 
-PAoutput <- dataPurification_SKTSP(sampleTreeRaw = PAsampletree,
-                                   plotHeadRaw = PAplotheader,
+PAoutput <- dataPurification_SKTSP_PPPA(sampleTreeRaw = PAsampletree,
+                                   plotHeaderRaw = PAplotheader,
                                    treeDataRaw = PAtreedata)
 
 rm(list=ls())
@@ -119,7 +128,8 @@ Mistikoutput <- dataPurification_SKTSP_Mistic(compiledPlotData = plotheader,
                                           compiledTreeData = treedata)
 
 rm(list=ls())
-setwd("C:/Users/Yong Luo/Documents/PSPs/Data/Data/MB")
+setwd("~/PSPs/Data/Data/MB")
+setwd("C:/Users/yonluo/Documents/PSPs/MB/PSPcsv")
 source('~/GitHub/landwebNRV/landwebNRV/R/dataPurification_MBPSP.R')
 MBPSPDataRaw <- read.csv("MBPSP_2012_file1.csv",
                          header = TRUE,
@@ -147,5 +157,17 @@ MBTSPDataRaw <- read.csv("MB_TSP_FML1.csv",
 dd <- dataPurification_MBTSP(MBTSPDataRaw)
 
 
- 
+rm(list=ls())
+setwd("~/PSPs/NRCAN/NFI_PSP")
+source('~/GitHub/landwebNRV/landwebNRV/R/dataPurification_NFIPSP.R')
+lgptreeRaw <- read.csv("all_gp_ltp_tree.csv", header= TRUE, stringsAsFactor=F) %>%
+  data.table
+lgpHeaderRaw <- read.csv("all_gp_ltp_header.csv", header= TRUE, stringsAsFactor=F) %>%
+  data.table
+approxLocation <- read.csv("all_gp_climate_approx_loc.csv", header= TRUE, stringsAsFactor=F) %>%
+  data.table
+
+NFIoutput <- dataPurification_NFIPSP(lgptreeRaw = lgptree, 
+                                     lgpHeaderRaw = lgpHeader,
+                                     approxLocation = approxLocations)
 
